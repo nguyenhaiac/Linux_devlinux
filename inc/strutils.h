84e20e3f7a1aee92db9665f85ead85f3d1dde83e
hai <nguyentienhai@hotmail.com.vn>
2025-09-03 21:54:26 +0700

    change folder structure

--- inc/strutils.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRUTILS_H_
-#define STRUTILS_H_
-
-void str_reverse(char * Text);
-void swap_char(char * a, char* b);
-char* str_trim(char * Text);
-int str_len(char * Text);
-
-#endif // STRUTILS_H_
